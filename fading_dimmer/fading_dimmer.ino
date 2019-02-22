ad53de1a5697acec160a53a1d1cce5e3fa721cd1
sopjiang <34628121+sopjiang@users.noreply.github.com>
2019-02-21 22:38:53 -0600

    Create fading_dimmer.ino

---
+++ fading_dimmer/fading_dimmer.ino
@@ -0,0 +1,32 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // keeping frequency at 100 Hz
+  /*for(int i=0; i<100; i=i+10){
+    dimmer(100, i);
+  }
+  for(int i=100; i>0; i=i-10){
+    dimmer(100, i);
+  }*/
+
+  // changing frequency
+  for(int i=10; i<10000; i=i+10){
+    dimmer(i, 100);
+  }  
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+}
+
+void dimmer(int freq, int duty){
+  int period, onTime, offTime;
+  period = 1000 / freq;
+  onTime = period * duty / 100;
+  offTime = period - onTime;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(offTime);
+}
